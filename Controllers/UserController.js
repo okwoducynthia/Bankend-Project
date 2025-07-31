2d56c6193871c0be8c41cd42e9623f82068d7bfa
okwoducynthia <okwoducynthia@gmail.com>
2025-07-31 15:04:52 +0100

    update

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,29 +1,32 @@
+const UserModel = require("../Models/UserModel");
+const bcrypt = require("bcryptjs")
+
+
 const SignUpUsers = async (req, res) => {
-
-  const {firstName, lastName, email, phoneNumber, password, isAdmin} = req.body
+  const { firstName, lastName, email, phoneNumber, password, isAdmin } =
+    req.body;
   try {
-
     /// TO CHECK IF USER EXIST IN OUR DB UNDER USER COLLECTION
     const userExist = await UserModel.findOne({
-      email
-    })
+      email,
+    });
     if (userExist) {
       return res.status(405).json({
-        message: "User Already Exist"
-      })
+        message: "User Already Exist",
+      });
     }
 
     /// TO CREATE A NEW USER
     const createNewUser = await UserModel.create({
-      firstName, 
-      lastName, 
-      email, 
-      phoneNumber, 
-      password
-    })
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      password,
+    });
 
     /// SAVING EVERYTHING IN THE REQ.BODY TO THE DB
-    const userResult = await createNewUser.save()
+    const userResult = await createNewUser.save();
 
     ///WHERE AM RETURNING THE DATA IF SUCCESSFUL
     res.status(200).json({
@@ -32,18 +35,53 @@ const SignUpUsers = async (req, res) => {
       lastName: userResult.lastName,
       email: userResult.email,
       phoneNumber: userResult.phoneNumber,
-      password: userResult.password,
-      isAdmin: userResult.isAdmin
+      isAdmin: userResult.isAdmin,
     });
-  } 
-  
-  catch (error) {
+  } catch (error) {
     res.status(404).json({
-      message: "Failed to fetch data"
-    })
+      message: "Failed to fetch data",
+    });
   }
-}
+};
+
+
+const Login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    /// TO CHECK IF USER EXIST IN OUR DB UNDER USER COLLECTION
+    const userExist = await UserModel.findOne({
+      email,
+    });
+    if (!userExist) {
+      return res.status(405).json({
+        message: "Invalid User",
+      });
+    }
+
+    const validPassword = await bcrypt.compare(password, userExist.password);
+    if (!validPassword) {
+      return res.status(406).json({
+        message: "Invalid Password"
+      })
+    }
+
+    ///WHERE AM RETURNING THE DATA IF SUCCESSFUL
+    res.status(200).json({
+      _id: userExist._id,
+      firstName: userExist.firstName,
+      lastName: userExist.lastName,
+      email: userExist.email,
+      phoneNumber: userExist.phoneNumber,
+      isAdmin: userExist.isAdmin,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: "Login error",
+    });
+  }
+};
 
 module.exports = {
   SignUpUsers,
-}
+  Login
+};
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { SignUpUsers } = require("../Controllers/UserController");
+const { SignUpUsers, Login } = require("../Controllers/UserController");
 
 
 
 const router = express.Router();
 
-router.post("/", SignUpUsers);
+router.post("/Signup", SignUpUsers);
+
+router.post("/Login", Login);
 
 module.exports = router;
 // export default router
