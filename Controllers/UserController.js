fd10de45e3737b7ca3473e3c3f05d6a9839e6247
okwoducynthia <okwoducynthia@gmail.com>
2025-08-04 11:33:15 +0100

    User Signup and Login

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,87 +1,90 @@
-const UserModel = require("../Models/UserModel");
+const userModel = require("../models/UserModel");
 const bcrypt = require("bcryptjs")
 
 
-const SignUpUsers = async (req, res) => {
-  const { firstName, lastName, email, phoneNumber, password, isAdmin } =
-    req.body;
-  try {
-    /// TO CHECK IF USER EXIST IN OUR DB UNDER USER COLLECTION
-    const userExist = await UserModel.findOne({
-      email,
-    });
-    if (userExist) {
-      return res.status(405).json({
-        message: "User Already Exist",
-      });
+const SignupUser = async (req, res) => {
+
+    const {firstName, lastName, email, phoneNumber, password} = req.body;
+    try {
+
+        // === To check if task exist in the DB under task collection ===
+        const phoneExist = await userModel.findOne({email});
+        if(phoneExist){
+            res.status(405).json({
+                message: "email already exist"
+            })
+        };
+        if(password.length <= 5){
+            res.status(405).json({
+                message: "password too short"
+            })
+        };
+
+        //  === To create new task ===
+        const createNewUser = new userModel ({
+           firstName, lastName, phoneNumber, email, password
+        });
+
+        // === To save everything in the req.body to the DB ===
+        const userResult = await createNewUser.save();
+
+
+        // === To return data if successful ===
+        // res.status(200).json(taskResult)
+
+        // ** Alternative to the above **
+        res.status(200).json({
+            _id: userResult._id,
+           firstName: userResult.firstName,
+            lastName: userResult.lastName,
+            phoneNumber: userResult.phoneNumber,
+            email: userResult.email,
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: "Failed to create user",
+        });
     }
-
-    /// TO CREATE A NEW USER
-    const createNewUser = await UserModel.create({
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-      password,
-    });
-
-    /// SAVING EVERYTHING IN THE REQ.BODY TO THE DB
-    const userResult = await createNewUser.save();
-
-    ///WHERE AM RETURNING THE DATA IF SUCCESSFUL
-    res.status(200).json({
-      _id: userResult._id,
-      firstName: userResult.firstName,
-      lastName: userResult.lastName,
-      email: userResult.email,
-      phoneNumber: userResult.phoneNumber,
-      isAdmin: userResult.isAdmin,
-    });
-  } catch (error) {
-    res.status(404).json({
-      message: "Failed to fetch data",
-    });
-  }
-};
+}
 
 
 const Login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    /// TO CHECK IF USER EXIST IN OUR DB UNDER USER COLLECTION
-    const userExist = await UserModel.findOne({
-      email,
-    });
-    if (!userExist) {
-      return res.status(405).json({
-        message: "Invalid User",
-      });
-    }
 
-    const validPassword = await bcrypt.compare(password, userExist.password);
-    if (!validPassword) {
-      return res.status(406).json({
-        message: "Invalid Password"
-      })
-    }
+    const { email, password } = req.body;
+    try {
 
-    ///WHERE AM RETURNING THE DATA IF SUCCESSFUL
-    res.status(200).json({
-      _id: userExist._id,
-      firstName: userExist.firstName,
-      lastName: userExist.lastName,
-      email: userExist.email,
-      phoneNumber: userExist.phoneNumber,
-      isAdmin: userExist.isAdmin,
-    });
-  } catch (error) {
-    res.status(404).json({
-      message: "Login error",
-    });
-  }
-};
+        // === To check if task exist in the DB under task collection ===
+        const userExist = await userModel.findOne({email});
+        if(!userExist){
+            return res.status(405).json({
+                message: "user not found"
+            })
+        };
+
+        const validPassword = await bcrypt.compare(password, userExist.password)
+        if(!validPassword){
+            return res.status(405).json({
+                message: "invalid password"
+            })
+        };
+
+        // === To return data if successful ===
+        res.status(200).json({
+            message: "Log in successful",
+            _id: userExist._id,
+           firstName: userExist.firstName,
+            lastName: userExist.lastName,
+            phoneNumber: userExist.phoneNumber,
+            email: userExist.email,
+        });
+    } catch (error) {
+        res.status(404).json({
+            message: "login error",
+        });
+    }
+}
 
 module.exports = {
-  SignUpUsers,
-  Login
-};
+    SignupUser,
+    Login
+}
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { SignUpUsers, Login } = require("../Controllers/UserController");
+const {SignupUser, Login} = require("../Controllers/UserController");
 
 
 
 const router = express.Router();
 
-router.post("/Signup", SignUpUsers);
+router.post("/Signup", SignupUser);
 
 router.post("/Login", Login);
 
